08b06441f51f80ebb71251f153af896604f16354
Alsje <alsje@mernok.co.za>
2023-10-05 17:15:01 +0200

    added masked code

--- public/alsje_sketch/sketch1.js
+++ public/alsje_sketch/sketch1.js
@@ -1,27 +1,24 @@
 let img; // The image to explode
-let explosion = false; // Flag to track explosion state
+let isExploding = false; // Flag to track explosion state
 let explosionRadius = 0; // Radius of explosion
-let originalImage; // Original image copy
-let explosionX;
-let explosionY;
-let lastFrameTime;
-
+let explosionX, explosionY; // Position of the explosion
 
 function preload() {
   // Load your image
-  img = loadImage("assets/new.png");
+  img = loadImage("assets/maya.png");
 }
 
 function setup() {
   createCanvas(img.width, img.height);
-  originalImage = img.get();
   image(img, 0, 0);
 }
 
 function draw() {
-  if (explosion) {
-    loadPixels();
+  background(255);
 
+  if (isExploding) {
+    // Perform the explosion effect
+    loadPixels();
     for (let y = 0; y < height; y++) {
       for (let x = 0; x < width; x++) {
         let dx = x - explosionX;
@@ -32,8 +29,8 @@ function draw() {
           // Calculate the angle for dispersion effect
           let angle = atan2(dy, dx);
 
-          // Adjust the displacement value to control the dispersion
-          let displacement = random(0, explosionRadius);
+          // Calculate the displacement amount based on distance
+          let displacement = map(distance, 0, explosionRadius, 0, 10);
 
           // Calculate the new position
           let newX = x + cos(angle) * displacement;
@@ -44,7 +41,7 @@ function draw() {
           newY = constrain(newY, 0, height - 1);
 
           // Sample the pixel from the original image and set it at the new position
-          let originalColor = originalImage.get(newX, newY);
+          let originalColor = img.get(newX, newY);
           set(x, y, originalColor);
         }
       }
@@ -53,28 +50,22 @@ function draw() {
   }
 }
 
-function mouseDragged() {
-  explosion = true;
+function mousePressed() {
+  isExploding = true;
   explosionX = mouseX;
   explosionY = mouseY;
-
-  // Calculate the time difference between frames and use it to increase explosion radius
-  let deltaTime = millis() - lastFrameTime;
-  explosionRadius += deltaTime * 0.2; // Adjust the multiplier for the rate of increase
-
-  // Store the current timestamp as the last frame time
-  lastFrameTime = millis();
 }
 
 function mouseReleased() {
-  explosion = false;
-  image(originalImage, 0, 0); // Restore the original image when the mouse is released
-  explosionRadius = 0; // Reset the explosion radius
+  isExploding = false;
+  explosionRadius = 0;
 }
 
-function mousePressed() {
-  // Initialize lastFrameTime when the mouse is pressed
-  lastFrameTime = millis();
+function mouseDragged() {
+  // Increase the explosion radius while the mouse is dragged
+  let dx = mouseX - explosionX;
+  let dy = mouseY - explosionY;
+  explosionRadius = sqrt(dx * dx + dy * dy);
 }
 
 window.preload = preload;
@@ -83,3 +74,92 @@ window.draw = draw;
 window.mouseReleased = mouseReleased;
 window.mouseDragged = mouseDragged;
 window.mousePressed = mousePressed;
+
+///
+
+// let img; // The image to explode
+// let explosion = false; // Flag to track explosion state
+// let explosionRadius = 0; // Radius of explosion
+// let originalImage; // Original image copy
+// let explosionX;
+// let explosionY;
+// let lastFrameTime;
+
+// function preload() {
+//   // Load your image
+//   img = loadImage("assets/new.png");
+// }
+
+// function setup() {
+//   createCanvas(img.width, img.height);
+//   originalImage = img.get();
+//   image(img, 0, 0);
+// }
+
+// function draw() {
+//   if (explosion) {
+//     loadPixels();
+
+//     for (let y = 0; y < height; y++) {
+//       for (let x = 0; x < width; x++) {
+//         let dx = x - explosionX;
+//         let dy = y - explosionY;
+//         let distance = sqrt(dx * dx + dy * dy);
+
+//         if (distance < explosionRadius) {
+//           // Calculate the angle for dispersion effect
+//           let angle = atan2(dy, dx);
+
+//           // Adjust the displacement value to control the dispersion
+//           let displacement = random(0, explosionRadius);
+
+//           // Calculate the new position
+//           let newX = x + cos(angle) * displacement;
+//           let newY = y + sin(angle) * displacement;
+
+//           // Ensure the new position is within bounds
+//           newX = constrain(newX, 0, width - 1);
+//           newY = constrain(newY, 0, height - 1);
+
+//           // Sample the pixel from the original image and set it at the new position
+//           let originalColor = originalImage.get(newX, newY);
+//           set(x, y, originalColor);
+//         }
+//       }
+//     }
+//     updatePixels();
+//   }
+// }
+
+// function mouseDragged() {
+//   explosion = true;
+//   explosionX = mouseX;
+//   explosionY = mouseY;
+
+//   // Calculate the time difference between frames and use it to increase explosion radius
+//   let deltaTime = millis() - lastFrameTime;
+//   explosionRadius += deltaTime * 0.2; // Adjust the multiplier for the rate of increase
+
+//   // Store the current timestamp as the last frame time
+//   lastFrameTime = millis();
+// }
+
+// function mouseReleased() {
+//   explosion = false;
+//   image(originalImage, 0, 0); // Restore the original image when the mouse is released
+//   explosionRadius = 0; // Reset the explosion radius
+// }
+
+// function mousePressed() {
+//   // Initialize lastFrameTime when the mouse is pressed
+//   lastFrameTime = millis();
+// }
+
+// window.preload = preload;
+// window.setup = setup;
+// window.draw = draw;
+// window.mouseReleased = mouseReleased;
+// window.mouseDragged = mouseDragged;
+// window.mousePressed = mousePressed;
+
+///
