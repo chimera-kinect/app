01d3ef410ea912e46764d4b4b47fb07f75cf8368
mellifluus <massimiliano.ricci@code.berlin>
2023-10-19 16:47:39 +0200

    feat: kinect manager finished + data viewer

--- public/utils/KinectManager.js
+++ public/utils/KinectManager.js
@@ -3,10 +3,13 @@ class KinectManager {
     this.socketHost = 'ws://localhost:12345'
     this.socket = new WebSocket(this.socketHost)
     this.currentFrame = null
-    this.nFrames = 0
     this.isRead = false
+    this.firstFrameReceived = false
     this.frameWidth = 1002
     this.frameHeight = 658
+    this.canvasWidth = null
+    this.canvasHeight = null
+    this.pushThreshold = 40
 
     this.socket.addEventListener('open', (_event) => {
       console.log('Connected to localhost:12345.')
@@ -15,18 +18,8 @@ class KinectManager {
     this.socket.addEventListener('message', async (event) => {
       const buf = await event.data.arrayBuffer()
       this.currentFrame = new Uint8Array(buf)
-      // if (this.currentFrame.find(elem => elem >= 255)) {
-      //   console.log('Found touch')
-      // }
       this.isRead = false
-      this.nFrames += 1
-      console.log(this.nFrames)
-      // npy.load(buf, (npyObject) => {
-      //   this.currentFrame = npyObject.data
-      //   this.isRead = false
-      //   this.nFrames += 1
-      //   console.log(this.nFrames)
-      // })
+      if (!this.firstFrameReceived) this.firstFrameReceived = true
     })
   
     this.socket.addEventListener('error', (event) => {
@@ -45,6 +38,24 @@ class KinectManager {
 
   get isFrameRead() {
     return this.isRead
+  }
+
+  updateCanvasSize() {
+    this.canvasWidth = width
+    this.canvasHeight = height
+  }
+
+  isCoordinatePushed(x, y) {
+    // translates frame size to canvas size
+    const xRatio = this.frameWidth / this.canvasWidth
+    const yRatio = this.frameHeight / this.canvasHeight
+
+    const targetX = Math.floor(x * xRatio)
+    const targetY = Math.floor(y * yRatio)
+
+    if (targetX < 0 || targetX >= this.frameWidth || targetY < 0 || targetY >= this.frameHeight) return 0
+
+    return this.currentFrame[(targetY * this.frameWidth) + targetX] >= this.pushThreshold
   }
 }
 
