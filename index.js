938ceec65fdae467eca10c6c607d823d35735c09
mellifluus <massimiliano.ricci@hello-charles.com>
2023-10-24 16:18:18 +0200

    feat: player now plays a random sketch every 10 seconds using a mock serial binding

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express = require('express')
 const path = require('path')
 const fs = require('fs')
 
+require('dotenv').config()
+
 const app = express()
-const port = 6372
+const port = process.env.PORT 
 
 // Find all directories in the 'public' folder
 const publicPath = path.join(__dirname, 'public')
--- player.js
+++ player.js
@@ -1,4 +1,20 @@
 const webdriver = require('selenium-webdriver')
+// const { SerialPort } = require('serialport')
+const { ReadlineParser } = require("@serialport/parser-readline")
+const { MockBinding } = require('@serialport/binding-mock') // no arduino here lol
+const { SerialPortStream } = require('@serialport/stream')
+
+require('dotenv').config()
+
+const getSketchUrl = sketch => `http://localhost:${process.env.PORT}/${sketch}`
+
+const sketches = [
+  'lines',
+  'circles',
+  'alsje',
+  'view_frame'
+  //...
+]
 
 const chromeCapabilities = webdriver.Capabilities.chrome()
 const chromeOptions = {
@@ -12,7 +28,19 @@ const driver = new webdriver.Builder()
   .withCapabilities(chromeCapabilities)
   .build()
 
-driver.get('http://localhost:6372/lines')
-  .then(() => driver.sleep(15000))
-  .catch(err => console.log(err))
-  .finally(() => driver.quit())
+const emitRandomInteger = (port) => {
+  port.port.emitData(`${Math.floor(Math.random() * sketches.length)}\n`)
+  setTimeout(() => emitRandomInteger(port), 10000) // every ten seconds, navigate to a random sketch
+}
+
+MockBinding.createPort('/dev/LMAO', { echo: true, record: true }) // create fake serial port that echoes back whatever you send to it
+const port = new SerialPortStream({ binding: MockBinding, path: '/dev/LMAO', baudRate: 14400 })
+
+const parser = new ReadlineParser()
+port.pipe(parser).on('data', line => {
+  driver.get(getSketchUrl(sketches[parseInt(line)]))
+})
+
+port.on('open', () => {
+  emitRandomInteger(port)
+})
