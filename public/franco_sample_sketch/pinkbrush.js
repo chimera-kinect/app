beac528edd9eca31a059ba53e173bae563c4ee4b
mellifluus <massimiliano.ricci@code.berlin>
2023-10-26 15:21:07 +0200

    lol

--- public/franco_sample_sketch/pinkbrush.js
+++ public/franco_sample_sketch/pinkbrush.js
@@ -1,47 +1,51 @@
 import kinectManager from '../utils/KinectManager.js'
 
 let drawing = false;
-let brushSize = 10;
+let increasing = false;
+let brushSize = 50;
 let prevX, prevY, curX, curY; // Store the previous mouse position
 
 function setup() {
     createCanvas(windowWidth, windowHeight)
     kinectManager.updateCanvasSize()
-    kinectManager.pushThreshold = 60
+    kinectManager.pushThreshold = 30
     frameRate(60)
     background("white");
 }
-
 function draw() {
     if (!kinectManager.firstFrameReceived) return
-    // if (drawing) {
-    let found = false
-    for (let x = 0; x <= width; x += random(10, 20)) {    
-        for (let y = 0; y <= height; y += random(10, 20)) {
-            if (kinectManager.isCoordinatePushed(x, y)) {
-                curX = x
-                curY = y
-                found = true
-                fill(255, 192, 203, 100);
-                noStroke();
-                break
-            }
-        }
-        if (found) break
-    }
 
-    // Interpolate between previous and current mouse positions
-    for (let i = 0; i < 1; i += 0.05) {
+    const coords = kinectManager.detectTouch()
+    if (drawing) {
+      if (!coords) {
+        drawing = false
+        return
+      }
+      if (!increasing) {
+        brushSize = 50
+        prevX = coords.x
+        prevY = coords.y
+        requestAnimationFrame(increaseBrushSize)
+        increasing = true
+      }
+      curX = coords.x
+      curY = coords.y
+      // if (dist(curX, curY, prevX, prevY) > 200) return !!! this removes weird lerping problems, but you have to slow down drawing to not make the lines break. up to you
+      fill(255, 192, 203, 100)
+      noStroke()
+      for (let i = 0; i < 1; i += 0.05) {
         let interX = lerp(prevX, curX, i);
         let interY = lerp(prevY, curY, i);
         circle(interX, interY, brushSize);
-    }
+      }
 
-    // Update the previous mouse positions
-    prevX = curX
-    prevY = curY
-    // prevX = mouseX;
-    // prevY = mouseY;
+      prevX = curX
+      prevY = curY
+    } else if (!drawing && coords) {
+      drawing = true
+    } else {
+      increasing = false
+    }
   }
 
 
@@ -50,29 +54,17 @@ function windowResized() {
     kinectManager.updateCanvasSize()
   }
 
-function mousePressed() {
-  drawing = true;
-  brushSize = 10;
-  prevX = mouseX;
-  prevY = mouseY;
-  increaseBrushSize();
-}
-
-function mouseReleased() {
-  drawing = false;
-}
-
 function increaseBrushSize() {
   if (drawing) {
-    brushSize += 2;
-    requestAnimationFrame(increaseBrushSize);
+    brushSize += 0.5
+    requestAnimationFrame(increaseBrushSize)
   }
 }
 
 function keyPressed() {
   if (key === 'c' || key === 'C') {
     // Clear the canvas when the 'C' key is pressed
-    background("white");
+    background("white")
   }
 }
 
